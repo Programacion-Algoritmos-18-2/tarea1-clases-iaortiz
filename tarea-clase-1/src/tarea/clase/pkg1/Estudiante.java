65d623443903e980eebd56b8a9bf8366bf28970a
Israel Alejandro <isralejo21@hotmail.com>
2018-10-05 11:36:06 -0500

    Agregando trabajo 1

---
+++ tarea-clase-1/src/tarea/clase/pkg1/Estudiante.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea.clase.pkg1;
+
+/**
+ *
+ * @author isral
+ */
+public class Estudiante {
+    //Definici{on de variables
+    private String nombre;
+    private String apellido;
+    
+    //Métodos Gets y Sets para las variables
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+  
+}
--- tarea-clase-1/src/tarea/clase/pkg1/TareaClase1.java
+++ tarea-clase-1/src/tarea/clase/pkg1/TareaClase1.java
@@ -5,6 +5,8 @@
  */
 package tarea.clase.pkg1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author isral
@@ -15,7 +17,23 @@ public class TareaClase1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Objetos de tipo estudiante
+        Estudiante est1 = new Estudiante();
+        Estudiante est2 = new Estudiante();
+
+        Scanner entrada = new Scanner(System.in);
+        //Entrada de datos para el estudiante 1
+        System.out.println("Ingrese el nombre y apellido del estudiante 1");
+        est1.setNombre(entrada.nextLine());
+        est1.setApellido(entrada.nextLine());
+        //Entrada de datos para el estudiante 2
+        System.out.println("Ingrese el nombre y apellido del estudiante 2");
+        est2.setNombre(entrada.nextLine());
+        est2.setApellido(entrada.nextLine());
+        //Presentación
+        System.out.printf("El nombre y apellido del estudiante 1 es:\t%s\t%s\n", est1.getNombre(),est1.getApellido());
+        System.out.printf("El nombre y apellido del estudiante 2 es:\t%s\t%s\n", est2.getNombre(),est2.getApellido());
+
     }
-    
+
 }
